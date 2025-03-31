014bf6a184a51dfd3424d27715df04ef2e908aff
MarcoCipolletta <marcocipolletta1998@gmail.com>
2025-03-31 15:38:50 +0200

    Add sender email on server

--- server.ts
+++ server.ts
@@ -5,6 +5,12 @@ import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import AppServerModule from './src/main.server';
 import fs from 'fs';
+import bodyParser from 'body-parser';
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+import { log } from 'node:console';
+
+dotenv.config();
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
@@ -50,12 +56,71 @@ export function app(): express.Express {
         providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
       });
 
-      res.status(200).send(html);
+      return res.status(200).send(html);
     } catch (err) {
       // Forza il fallback al client-side rendering anche in caso di errore
       const html = fs.readFileSync(indexHtml).toString();
       // res.status(404).send(html); // status 200, così Angular sul client può fare il routing
-      res.status(404).redirect('404');
+      return res.status(404).redirect('404');
+    }
+  });
+
+  //  Servizio e api per inviare email
+  server.use(bodyParser.json());
+
+  server.post('/api/:lang/contact', async (req, res) => {
+    const { lang } = req.params; // "it" o "en"
+    const { name, email, message } = req.body;
+
+    if (!name || !email || !message) {
+      return res.status(400).json({
+        error:
+          lang === 'it'
+            ? 'Tutti i campi sono obbligatori'
+            : 'All fields are required',
+      });
+    }
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env['EMAIL_USER'],
+        pass: process.env['EMAIL_PASS'],
+      },
+    });
+
+    try {
+      // Email a me
+      await transporter.sendMail({
+        from: `"${name}" <${email}>`,
+        to: process.env['EMAIL_USER'],
+        subject: `Nuovo messaggio dal portfolio da ${name}`,
+        text: `Messaggio da ${name} (${email}):\n\n${message}`,
+      });
+
+      // Risposta automatica all’utente
+      const replyText =
+        lang === 'it'
+          ? `Ciao ${name},\n\nGrazie per il tuo messaggio! Ti risponderò al più presto.\n\nMarco`
+          : `Hi ${name},\n\nThank you for your message! I’ll get back to you soon.\n\nMarco`;
+
+      await transporter.sendMail({
+        from: `"Marco Cipolletta" <${process.env['EMAIL_USER']}>`,
+        to: email,
+        subject: lang === 'it' ? 'Conferma ricezione' : 'Message received',
+        text: replyText,
+      });
+
+      return res.status(200).json({ success: true });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({
+          error:
+            lang === 'it'
+              ? 'Errore durante l’invio dell’email'
+              : 'Error sending email',
+        });
     }
   });
 
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 import { LanguageService } from '../../services/translate/language.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -11,7 +12,8 @@ export class ContactComponent {
   constructor(
     private meta: Meta,
     private title: Title,
-    private langSvc: LanguageService
+    private langSvc: LanguageService,
+    private http: HttpClient
   ) {
     this.meta.removeTag('name="robots"');
     this.langSvc.lang$.subscribe((lang) => {
@@ -19,5 +21,30 @@ export class ContactComponent {
         `Marco Cipolletta - ${lang === 'it' ? 'Contatti' : 'Contacts'}`
       );
     });
+  }
+
+  provaIt() {
+    this.http
+      .post('/api/it/contact', {
+        name: 'Marco it',
+        email: 'marcocipolletta2@gmail.com',
+        message: 'Prova in italianooooo',
+      })
+      .subscribe({
+        next: () => alert('Messaggio inviato!'),
+        error: () => alert('Errore durante l’invio. Riprova.'),
+      });
+  }
+  provaEn() {
+    this.http
+      .post('/api/en/contact', {
+        name: 'Marco en',
+        email: 'marcocipolletta2@gmail.com',
+        message: 'Prova in inglese',
+      })
+      .subscribe({
+        next: () => alert('Messaggio inviato!'),
+        error: () => alert('Errore durante l’invio. Riprova.'),
+      });
   }
 }
