5a00e5db16d6508a58362b8d44e7ed772197e148
MarcoCipolletta <marcocipolletta1998@gmail.com>
2025-04-04 14:51:26 +0200

    add page 404

--- src/app/pages/not-found/not-found.module.ts
+++ src/app/pages/not-found/not-found.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { NotFoundRoutingModule } from './not-found-routing.module';
 import { NotFoundComponent } from './not-found.component';
-
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
-  declarations: [
-    NotFoundComponent
-  ],
-  imports: [
-    CommonModule,
-    NotFoundRoutingModule
-  ]
+  declarations: [NotFoundComponent],
+  imports: [CommonModule, NotFoundRoutingModule, TranslateModule.forChild()],
 })
-export class NotFoundModule { }
+export class NotFoundModule {}
